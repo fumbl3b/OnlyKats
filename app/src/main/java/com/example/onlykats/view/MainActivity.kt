cf2682d0643cdfa7fca5f01fc16f601b8ab7b553
Harry Winkler <harrywinkler@Harrys-MacBook-Air.local>
2021-09-02 11:16:53 -0400

    Initial commit

---
+++ app/src/main/java/com/example/onlykats/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.onlykats.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.onlykats.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
