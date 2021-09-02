35d5121e3c275b3d69e37cd6736b1e68cbbd08eb
harrywinkler <harry@fumblebee.site>
2021-09-02 13:03:49 -0400

    Completed fragment skeletons

--- app/src/main/java/com/example/onlykats/view/HomeActivity.kt
+++ app/src/main/java/com/example/onlykats/view/HomeActivity.kt
@@ -2,12 +2,17 @@ package com.example.onlykats.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.onlykats.R
 import com.example.onlykats.databinding.ActivityMainBinding
 
 class HomeActivity : AppCompatActivity() {
 
+    // by is a delegate lazy {}
+
+    // private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var binding: ActivityMainBinding
+
+    // val is a runtime constant
+    // var is mutable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
