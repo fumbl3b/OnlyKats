d1add1ba22e47a2d56c73baca93dace855888e84
harrywinkler <harry@fumblebee.site>
2021-09-03 11:27:53 -0400

    fixed navigation_graph

--- app/src/main/java/com/example/onlykats/view/BrowseFragment.kt
+++ app/src/main/java/com/example/onlykats/view/BrowseFragment.kt
@@ -2,7 +2,9 @@ package com.example.onlykats.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.onlykats.R
 import com.example.onlykats.databinding.FragmentBrowseBinding
 
@@ -14,7 +16,18 @@ class BrowseFragment : Fragment(R.layout.fragment_browse) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentBrowseBinding.bind(view)
+        initViews()
 
+    }
 
+    private fun initViews() = with(binding) {
+        detailButton.setOnClickListener {
+            findNavController().navigate(R.id.action_browseFragment_to_detailFragment)
+            Toast.makeText(context,"Details",Toast.LENGTH_SHORT).show()
+        }
+        settingsButton.setOnClickListener {
+            findNavController().navigate(R.id.action_browseFragment_to_settingsFragment)
+            Toast.makeText(context,"Settings",Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/onlykats/view/HomeActivity.kt
+++ app/src/main/java/com/example/onlykats/view/HomeActivity.kt
@@ -2,24 +2,17 @@ package com.example.onlykats.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.onlykats.databinding.ActivityMainBinding
+import com.example.onlykats.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
-    // by is a delegate lazy {}
-
-    // private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private lateinit var binding: ActivityMainBinding
-
-    // val is a runtime constant
-    // var is mutable
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
     }
-
 
 }
